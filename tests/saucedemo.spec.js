dbd9b1b54dfa02ed6a87a6a5e6615f353eed80f4
IsratAra17 <smisratara25@gmail.com>
2025-11-16 22:29:52 +0600

    login test

--- tests/saucedemo.spec.js
+++ tests/saucedemo.spec.js
@@ -1,5 +1,15 @@
 const { test, expect } = require('@playwright/test');
+test.beforeEach(async ({ page }) => {
+  await page.goto('https://www.saucedemo.com/');
+  await page.fill('#user-name', 'standard_user');
+  await page.fill('#password', 'secret_sauce');
+  await page.click('#login-button');
 
+  // Verify login
+  await expect(page).toHaveURL(/inventory/);
+});
 
-
-  
+// ----- TEST 1: LOGIN -----
+test('User logs in successfully', async ({ page }) => {
+  await expect(page).toHaveURL(/inventory/);
+});
