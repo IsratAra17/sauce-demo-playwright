df91a74ebd7dc038dfde91377c6d5f5cef49ae24
IsratAra17 <smisratara25@gmail.com>
2025-11-16 22:40:21 +0600

    Add test for adding a product to cart and verifying product name

--- tests/saucedemo.spec.js
+++ tests/saucedemo.spec.js
@@ -13,3 +13,22 @@ test.beforeEach(async ({ page }) => {
 test('User logs in successfully', async ({ page }) => {
   await expect(page).toHaveURL(/inventory/);
 });
+
+
+
+test('Add a product to cart and verify it', async ({ page }) => {
+  
+  // Get the first product name
+  const productName = await page.textContent('.inventory_item_name');
+
+  // Add to cart
+  await page.click('button[id*="add-to-cart"]');
+
+  // Open cart
+  await page.click('.shopping_cart_link');
+
+  // Verify product in cart
+  const cartProduct = await page.textContent('.inventory_item_name');
+
+  expect(cartProduct.trim()).toBe(productName.trim());
+});
